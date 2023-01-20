5d4e04d099859e1e8734696dd15943b6034d76b6
Klinton <klinton@Klintons-MacBook-Pro.local>
2023-01-20 11:21:36 +0700

    connect db to sqlite and refactor handler func

--- main.go
+++ main.go
@@ -4,16 +4,36 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 func main() {
+	db, err := gorm.Open(sqlite.Open("book.db"), &gorm.Config{})
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	db.AutoMigrate(&Book{})
+
+	handler := newHandler(db)
+
 	r := gin.New()
 
-	r.GET("/books", listBooksHandler)
-	r.POST("/books", createBookHandler)
-	r.DELETE("/books/:id", deleteBookHandler)
+	r.GET("/books", handler.listBooksHandler)
+	r.POST("/books", handler.createBookHandler)
+	r.DELETE("/books/:id", handler.deleteBookHandler)
 
 	r.Run()
+}
+
+type Handler struct {
+	db *gorm.DB
+}
+
+func newHandler(db *gorm.DB) *Handler {
+	return &Handler{db}
 }
 
 type Book struct {
@@ -22,39 +42,46 @@ type Book struct {
 	Author string `json:"author"`
 }
 
-var books = []Book{
-	{ID: "1", Title: "Harry Potter", Author: "J. K. Rowling"},
-	{ID: "2", Title: "The Lord of the Rings", Author: "J. R. R. Tolkien"},
-	{ID: "3", Title: "The Wizard of Oz", Author: "L. Frank Baum"},
+func (h *Handler) listBooksHandler(c *gin.Context) {
+	var books []Book
+
+	if result := h.db.Find(&books); result.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": result.Error.Error(),
+		})
+	}
+
+	c.JSON(http.StatusOK, &books)
 }
 
-func listBooksHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, books)
-}
+func (h *Handler) createBookHandler(c *gin.Context) {
+	var newBook Book
 
-func createBookHandler(c *gin.Context) {
-	var book Book
-
-	if err := c.ShouldBindJSON(&book); err != nil {
+	if err := c.ShouldBindJSON(&newBook); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"error": err.Error(),
 		})
 		return
 	}
 
-	books = append(books, book)
+	if result := h.db.Create(&newBook); result.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": result.Error.Error(),
+		})
+		return
+	}
 
-	c.JSON(http.StatusCreated, book)
+	c.JSON(http.StatusCreated, &newBook)
 }
 
-func deleteBookHandler(c *gin.Context) {
+func (h *Handler) deleteBookHandler(c *gin.Context) {
 	id := c.Param("id")
 
-	for i, a := range books {
-		if a.ID == id {
-			books = append(books[:i], books[i+1:]...)
-			break
-		}
+	if result := h.db.Delete(&Book{}, id); result.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": result.Error.Error(),
+		})
+		return
 	}
 
 	c.Status(http.StatusNoContent)
